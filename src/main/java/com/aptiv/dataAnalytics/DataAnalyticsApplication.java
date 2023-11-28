1e95151cc24b7fe4c877a23f57be16e2b28f2ec2
admin <anasszeroual09@gmail.com>
2023-11-28 13:49:38 +0100

    re

---
+++ src/main/java/com/aptiv/dataAnalytics/DataAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.aptiv.dataAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aptiv/dataAnalytics/DataAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptiv.dataAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
